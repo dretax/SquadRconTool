df7f80530c99b05cfac1ca063fadfaf9676fa4b3
DreTaX <dretax14@gmail.com>
2019-11-21 11:38:23 +0100

    Modify domainipcontainer.

--- SquadRconServer/ServerContainer/DomainIPContainer.cs
+++ SquadRconServer/ServerContainer/DomainIPContainer.cs
@@ -5,34 +5,24 @@ namespace SquadRconServer.ServerContainer
     public class DomainIPContainer
     {
         private readonly IPAddress _ip;
-        private readonly string _domain;
-        private readonly bool _isdomain;
 
         public DomainIPContainer(string domain)
         {
-            _domain = domain;
-            _isdomain = true;
+            var address = Dns.GetHostAddresses(domain);
+            if (address.Length > 0)
+            {
+                _ip = address[0];
+            }
         }
 
         public DomainIPContainer(IPAddress ip)
         {
             _ip = ip;
-            _isdomain = false;
-        }
-
-        public string Domain
-        {
-            get { return _domain; }
         }
 
         public IPAddress IP
         {
             get { return _ip; }
-        }
-
-        public bool IsDomain
-        {
-            get { return _isdomain; }
         }
     }
 }
